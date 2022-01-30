ba3f5ae55d0b1574727f68e822ad7968d9fd9391
mohamed-Hamdy97 <42852127+mohamed-Hamdy97@users.noreply.github.com>
2022-01-30 15:45:05 +0200

    build ui of details view

--- native-mobile-ios-development-learning/Learning-Objective-C/Project-demos/MoviesApp/MoviesApp/DetailsViewController.h
+++ native-mobile-ios-development-learning/Learning-Objective-C/Project-demos/MoviesApp/MoviesApp/DetailsViewController.h
@@ -6,10 +6,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Model/Movie.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
+
+@property Movie *movie;
+
+@property (weak, nonatomic) IBOutlet UIImageView *movieImage;
+@property (weak, nonatomic) IBOutlet UILabel *movieTitle;
+@property (weak, nonatomic) IBOutlet UILabel *movieReleaseYear;
+@property (weak, nonatomic) IBOutlet UILabel *movieRating;
+@property (weak, nonatomic) IBOutlet UILabel *movieGenre;
 
 @end
 
